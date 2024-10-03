149ded5ef8a3c21d6e6bc4dcb20b18ad0a3e91d8
Subilan <suyuanliang1995@gmail.com>
2024-10-03 17:05:12 +0800

    chore: initialize templates

---
+++ pages/docs/docs.vue
@@ -0,0 +1,15 @@
+<template>
+  123
+</template>
+
+<script lang="ts" setup>
+const docPath = useRoute().params.docPath as string;
+
+async function fetchDocumentation() {
+  const res = await $fetch(`/api/get-doc?path=${btoa(docPath)}`)
+
+  if (window) console.log(res)
+}
+
+fetchDocumentation();
+</script>
