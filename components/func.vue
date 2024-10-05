ebff549a3d4ffbdd1708ae4e7d7ae5d9ff426fcb
Subilan <suyuanliang1995@gmail.com>
2024-10-05 20:41:45 +0800

    chore: more features on `func`

--- components/func.vue
+++ components/func.vue
@@ -12,10 +12,10 @@
         <div class="param-title">
           <component :is="singlePage ? 'h3' : 'h4'" :id="getParamId(x.name, object.name)"><code>{{ x.name }}</code>
           </component>
-          <div class="param-type" v-html="'<code>' + x.types.map(y => `<span class='type'>${y}</span>`).join(' | ') + '</code>'"/>
+          <div class="param-type" v-html="`<code>${buildSpansFromTypes(x.types).join(' | ')}</code>`"/>
           <div class="param-props">
             <template v-for="y in ['positional', 'settable', 'variadic', 'required']">
-              <span class="prop" v-if="x[y]">{{ Cap(y) }}</span>
+              <span class="prop" v-if="x[y]">{{ capitalize(y) }}</span>
             </template>
           </div>
         </div>
@@ -48,16 +48,37 @@ const props = defineProps({
   }
 });
 
-function Cap(str: string) {
+/**
+ * 使 str 首字母大写
+ * @param str
+ */
+function capitalize(str: string) {
   return `${str[0].toUpperCase()}${str.slice(1, str.length)}`;
 }
 
+/**
+ * 根据一组字符串返回对应一组的 span 元素
+ * @param types 包含类型的字符串
+ */
+function buildSpansFromTypes(types: string[]) {
+  return types.map(y => `<span class="typ typ-${y}">${y}</span>`);
+}
+
+/**
+ * 根据组件的相关配置与 name 的值，返回其 anchor 的内容
+ * @param name 参数名称
+ */
 function getFunctionNameId(name: string) {
   if (props.singlePage) return 'summary';
   if (props.paramIdPrefix.length > 0) return `${props.paramIdPrefix}-${name}`;
   return `definitions-${name}`;
 }
 
+/**
+ * 根据组件的相关配置与 name 和 objectName 的值，返回其 anchor 的内容
+ * @param name 参数名称
+ * @param objectName 函数名称
+ */
 function getParamId(name: string, objectName = '') {
   if (props.singlePage) return `parameters-${name}`;
   if (props.paramIdPrefix.length > 0)
@@ -72,7 +93,8 @@ function buildFunctionDefinition(object: Func) {
   const longParam = object.params.length >= 3;
 
   if (object.self) res += 'self.';
-  if (object.path.length > 0) res += object.path.join('.') + '.';
+  // else: should only be one of `self.` and path.
+  else if (object.path.length > 0) res += object.path.join('.') + '.';
 
   res += object.name;
   res += '(';
@@ -85,7 +107,7 @@ function buildFunctionDefinition(object: Func) {
         r += `${x.name}: `
       }
 
-      r += `<span class="type">${x.types.join(' | ')}</span>`;
+      r += buildSpansFromTypes(x.types).join(' | ');
       return r;
     }).join(`,${longParam ? '\n  ' : ' '}`);
     res += longParam ? '\n' : ' ';
@@ -93,7 +115,7 @@ function buildFunctionDefinition(object: Func) {
   res += ')';
 
   if (object.returns.length > 0) {
-    res += ` -> ${object.returns.join(' | ')}`;
+    res += ` -> ${buildSpansFromTypes(object.returns).join(' | ')}`;
   }
 
   return res;
