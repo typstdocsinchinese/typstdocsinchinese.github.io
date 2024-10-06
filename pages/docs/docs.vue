ea53a2698f2b4d097bd49ea32337b8f7571373c5
Subilan <suyuanliang1995@gmail.com>
2024-10-06 17:57:44 +0800

    feat: build `original` blocks structure dyn

--- pages/docs/docs.vue
+++ pages/docs/docs.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="page-html" v-if="pageType === PageType.HTML" v-html="htmlBody"></div>
-  <div class="page-category" v-else-if="pageType === PageType.Category">
-    <Category :value="object"/>
-  </div>
-  <div class="page-type" v-else-if="pageType === PageType.Type">
-    <Type :value="object"/>
-  </div>
-  <div class="page-func" v-else-if="pageType === PageType.Func">
-    <Func :value="object"/>
-  </div>
-  <div class="page-group" v-else-if="pageType === PageType.Group">
-    <Group :value="object"/>
-  </div>
-  <div class="page-symbols" v-else-if="pageType === PageType.Symbols">
-    <Symbols :value="object"/>
-  </div>
+  <article ref="arti">
+    <div class="page-html" v-if="pageType === PageType.HTML" v-html="htmlBody"></div>
+    <div class="page-category" v-else-if="pageType === PageType.Category">
+      <Category :value="object"/>
+    </div>
+    <div class="page-type" v-else-if="pageType === PageType.Type">
+      <Type :value="object"/>
+    </div>
+    <div class="page-func" v-else-if="pageType === PageType.Func">
+      <Func :value="object"/>
+    </div>
+    <div class="page-group" v-else-if="pageType === PageType.Group">
+      <Group :value="object"/>
+    </div>
+    <div class="page-symbols" v-else-if="pageType === PageType.Symbols">
+      <Symbols :value="object"/>
+    </div>
+  </article>
 </template>
 
 <script lang="ts" setup>
@@ -23,7 +25,6 @@ import {
   type Func,
   type Group,
   type Nullable,
-  type Outline,
   PageType,
   type Route,
   type Symbols,
@@ -38,6 +39,8 @@ const pageType = ref<PageType>(PageType.Unknown);
 const title = ref('');
 const description = ref('');
 const outline = useOutline();
+
+const arti = ref<HTMLDivElement | null>(null);
 
 const htmlBody = ref('');
 const object = ref<Nullable<Func | Category | Type | Group | Symbols>>(null);
@@ -68,5 +71,28 @@ async function fetchDocumentation() {
   }
 }
 
-fetchDocumentation();
+function wrapOriginalContents() {
+  if (document) {
+    console.log(document.querySelectorAll('p + original, .previewed-code + original, .info-box + original'))
+
+    document.querySelectorAll('p + original, .previewed-code + original, .info-box + original, ol + original, ul + original').forEach(e => {
+      const previous = e.previousElementSibling;
+      if (!previous) return;
+      const div = document.createElement('div');
+      div.classList.add('with-original');
+      div.appendChild(previous.cloneNode(true));
+      const newE = e.cloneNode(true) as HTMLDivElement;
+      newE.innerText = newE.innerText.trim();
+      newE.innerHTML = '<span>原文</span>' + `<div class="content">${newE.innerHTML}</div>`;
+      div.appendChild(newE);
+      e.parentNode?.insertBefore(div, e);
+      e.remove();
+      previous.remove();
+    })
+  }
+}
+
+await fetchDocumentation();
+
+watch(arti, () => wrapOriginalContents());
 </script>
